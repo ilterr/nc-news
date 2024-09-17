b683364e06fd1fe5172b32e36bcc693edd2cc552
Ilter Ahmet <ilterahmet93@gmail.com>
2024-09-17 12:01:10 +0100

    add css styling and refactor jsx html elements

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 const ArticleCard = ({ article }) => {
-  console.log(article, "<---- LOG");
   return (
-    <section className="articleCard">
-      <h2>{article.title}</h2>
-      <img src={article.articles_img_url} width={100} height={100} />
-      <p>{article.body}</p>
-      <button>See comments</button>
-    </section>
+    <article className="articleCard">
+      <div className="articleBody">
+        <div className="articleHeader">
+          <div className="author">{article.author}</div>
+          <h2 className="title">{article.title}</h2>
+          <div className="topic">{article.topic}</div>
+          <div className="createdAt">{article.created_at}</div>
+        </div>
+        <p>{article.body}</p>
+        <button type="button">See comments ({article.comment_count})</button>
+      </div>
+      <img src={article.article_img_url} alt={`Image for ${article.title}`} />
+    </article>
   );
 };
 
